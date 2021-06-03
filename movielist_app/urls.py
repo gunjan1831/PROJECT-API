400a124bd48af7ea60b6666c8f77bf56fdac29e4
gunjan1831 <gunjanpurwar1832000@gmail.com>
2021-06-03 16:24:25 +0530

    lets see

--- movielist_app/urls.py
+++ movielist_app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
-from movielist_app.views import movie_list
+from movielist_app.views import movie_list,movie_details
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+
     path('lists/', movie_list,name='movie_list'),
+    path('<int:pk>',movie_details,name='movie_details'),
 ]
