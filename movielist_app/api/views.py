901343ae2b318315d2988f7589945f3d8335c3fd
gunjan1831 <gunjanpurwar1832000@gmail.com>
2021-06-08 15:52:06 +0530

    single streaming details added StreamPlatformAV

--- movielist_app/api/views.py
+++ movielist_app/api/views.py
@@ -60,6 +60,30 @@ class WatchDetailsAV(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class StreamDetailsAV(APIView):
+
+    def get(self,request,pk):
+        try:
+             stream = StreamPlatform.objects.get(pk=pk)
+        except StreamPlatform.DoesNotExist:
+             return Response({'Error':'Streaming Platform Not Found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = StreamPlatformSerializer(stream)
+        return Response(serializer.data)
+
+    def put(self,request,pk):
+            stream = StreamPlatform.objects.get(pk=pk)
+            serializer  = StreamPlatformSerializer(stream, data=request.data)
+            if serializer.is_valid():
+                  serializer.save()
+                  return Response(serializer.data)
+            else:
+               return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,pk):
+        stream = StreamPlatform.objects.get(pk=pk)
+        stream.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 # @api_view(['GET','POST'])
 # def movie_list(request):
 #
